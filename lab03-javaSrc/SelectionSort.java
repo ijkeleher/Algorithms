4e70235c460c7500047e90409fb85816ed82d8e0
Inci <inci90@users.noreply.github.com>
2018-08-06 22:52:11 +1000

    updated selection sort code

--- lab03-javaSrc/SelectionSort.java
+++ lab03-javaSrc/SelectionSort.java
@@ -17,32 +17,22 @@ public class SelectionSort extends SortAlgorithm
     public void sort(int[] array) {
 
 		//init variables for storing min
-		int minValue = array[0];
-		int minIndex = 0;
-		System.out.print("minvalue is "+minValue+"\n");
 
-		//iterate through and find index of minValue
 		for (int i = 0; i < array.length-1; i++) {
-			//store new min values
-			if(array[i] < minValue){
-				minIndex = i;
-				minValue = array[i];
-				System.out.println("new min is " + minValue);
-			}
-
-			//swap values
-            for (int j = i+1; j < array.length; j++) {
-                // check if we need to swap if so then swap
-                if (array[j] < array[i]) {
-                    Integer temp = array[j];
-                    array[j] = array[i];
-                    array[i] = temp;
-					System.out.println("swapping "+array[i]+" and "+array[j]);
-
+			int minValue = Integer.MAX_VALUE;
+			int minIndex = i+1;
+            for (int j = i; j < array.length; j++) {
+				//iterate through and find index of minValue
+                if (array[j] < minValue) {
+                    minIndex = j;
+                    minValue = array[j];
                 }
             }
-        }
-
+			//swap values
+			int temp = array[i];
+			array[i] = array[minIndex];
+			array[minIndex] = temp;
+			}
 
 
     } // end of sort()
